501a65d8af144e15b2b51132a736584c9ee7e3ba
tpxxn <351765204@qq.com>
2018-10-20 17:30:56 +0800

    添加项目文件。

---
+++ GMS2ManualIndex/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GMS2ManualIndex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
